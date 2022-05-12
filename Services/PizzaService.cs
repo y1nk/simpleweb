84620b973da51141dcca56f843d1ca85eaf9f281
y1nk <yas.valentino@gmail.com>
2022-05-12 22:43:31 +0100

    web api tut

---
+++ Services/PizzaService.cs
@@ -0,0 +1,48 @@
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Services;
+
+public static class PizzaService
+{
+    static List<Pizza> Pizzas { get; }
+    static int nextId = 6;
+    static PizzaService()
+    {
+        Pizzas = new List<Pizza>
+        {
+            new Pizza { Id = 1, Name = "Classic Italian", IsGlutenFree = false },
+            new Pizza { Id = 2, Name = "Veggie", IsGlutenFree = true },
+            new Pizza { Id = 3, Name = "Margherita", IsGlutenFree = false },
+            new Pizza { Id = 4, Name = "Asian Sepcial", IsGlutenFree = false },
+            new Pizza { Id = 5, Name = "Pepperoni Special", IsGlutenFree = false }
+        };
+    }
+
+    public static List<Pizza> GetAll() => Pizzas;
+
+    public static Pizza? Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
+
+    public static void Add(Pizza pizza)
+    {
+        pizza.Id = nextId++;
+        Pizzas.Add(pizza);
+    }
+
+    public static void Delete(int id)
+    {
+        var pizza = Get(id);
+        if(pizza is null)
+            return;
+
+        Pizzas.Remove(pizza);
+    }
+
+    public static void Update(Pizza pizza)
+    {
+        var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
+        if(index == -1)
+            return;
+
+        Pizzas[index] = pizza;
+    }
+}
